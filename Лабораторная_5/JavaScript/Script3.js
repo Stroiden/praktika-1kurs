16c862493f8a4d1a40dd76c645b3692eef6140e7
Maksim Kuzmin <27maksim0902@gmail.com>
2021-06-12 15:41:49 +0400

    added third part of lab5

---
+++ Лабораторная_5/JavaScript/Script3.js
@@ -0,0 +1,344 @@
+var a = 1;
+var b = 0;
+var c = -3;
+
+//задание 1
+if (a == 0) { 
+    alert ('При ' + a + ' условие верно!');
+} else {
+    alert ('При ' + a + ' условие неверно!');
+}
+
+if (b == 0) { 
+    alert ('При ' + b + ' условие верно!'); 
+} else {
+    alert ('При ' + b + ' условие неверно!');
+}
+
+if (c == 0) {
+    alert ('При ' + c + ' условие верно!');
+} else {
+    alert ('При ' + c + ' условие неверно!');
+}
+
+
+//задание 2 
+if (a > 0) {
+    alert ('При ' + a + ' условие верно!');
+} else {
+    alert ('При ' + a + ' условие неверно!');
+}
+
+if (b > 0) {
+    alert ('При ' + b + ' условие верно!');
+} else {
+    alert ('При ' + b + ' условие неверно!');
+}
+
+if (c > 0) {
+    alert ('При ' + c + ' условие верно!')
+} else {
+    alert ('При ' + c + ' условие неверно!');
+}
+
+
+//задание 3
+if (a < 0) {
+    alert ('При ' + a + ' условие верно!');
+} else {
+    alert ('При ' + a + ' условие неверно!');
+}
+
+if (b < 0) {
+    alert ('При ' + b + ' условие верно!');
+} else {
+    alert ('При ' + b + ' условие неверно!');
+}
+
+if (c < 0) {
+    alert ('При ' + c + ' условие верно!');
+} else {
+    alert ('При ' + c + ' условие неверно!');
+}
+
+//задание 4
+if (a >= 0) {
+    alert ('При ' + a + ' условие верно!');
+} else {
+    alert ('При ' + a + ' условие неверно!');
+}
+
+if (b >= 0) {
+    alert ('При ' + b + ' условие верно!');
+} else {
+    alert ('При ' + b + ' условие неверно!');
+}
+
+if (c >= 0) {
+    alert ('При ' + c + ' условие верно!');
+} else {
+    alert ('При ' + c + ' условие неверно!');
+}
+
+//задание 5
+if (a <= 0) {
+    alert ('При ' + a + ' условие верно!');
+} else {
+    alert ('При ' + a + ' условие неверно!');
+}
+
+if (b <= 0) {
+    alert ('При ' + b + ' условие верно!');
+} else {
+    alert ('При ' + b + ' условие неверно!');
+}
+
+if (c <= 0) {
+    alert ('При ' + c + ' условие верно!');
+} else {
+    alert ('При ' + c + ' условие неверно!');
+}
+
+//задание 6
+if (a != 0) {
+    alert ('При ' + a + ' условие верно!');
+} else {
+    alert ('При ' + a + ' условие неверно!');
+}
+
+if (b != 0) { 
+    alert ('При ' + b + ' условие верно!');
+} else {
+    alert ('При ' + b + ' условие неверно!');
+}
+
+if (c != 0) {
+    alert ('При ' + c + ' условие верно!');
+} else {
+    alert ('При ' + c + ' условие неверно!');
+}
+
+
+//Задание 7
+var d = 'test';
+var e = 'тест';
+var f = 3;
+
+if (d === 'test') {
+    alert ('При ' + d + ' условие верно!');
+} else {
+    alert ('При ' + d + ' условие неверно!');
+}
+
+if (e === 'test') {
+    alert ('При ' + e + ' условие верно!');
+} else {
+    alert ('При ' + e + ' условие неверно!');
+}
+
+if (f == 'test') {
+    alert ('При ' + f + ' условие верно!');
+} else {
+    alert ('При ' + f + ' условие неверно!');
+}
+
+
+//Задание 8 
+var g = '1';
+var h = 1;
+var i = -3;
+
+if (g === '1') {
+    alert ('При ' + g + ' условие верно!');
+} else {
+    alert ('При ' + g + ' условие неверно!');
+}
+
+if (h === '1') {
+    alert ('При ' + h + ' условие верно!');
+} else {
+    alert ('При ' + h + ' условие неверно!');
+}
+
+if (i == '1') {
+    alert ('При ' + i + ' условие верно!');
+} else {
+    alert ('При ' + i + ' условие неверно!');
+}
+
+
+
+var test = true;
+var test1 = false;
+
+// Задание 9
+if (test == true) alert('При ' + test + ' условие верно!'); 
+else alert('При ' + test + ' условие неверно!');
+
+if (test1 == false) alert('При ' + test1 + ' условие верно!'); 
+else alert('При ' + test1 + ' условие неверно!');
+
+
+// Задание 10
+if (!test) alert('При ' + test + ' условие верно!'); 
+else alert('При ' + test + ' условие неверно!');
+
+
+if (test1 !== false) alert('При ' + test1 + ' условие верно!'); 
+else alert('При ' + test1 + ' условие неверно!');
+if (!test1) alert('При ' + test1 + ' условие верно!'); 
+else alert('При ' + test1 + ' условие неверно!');
+
+
+
+var a1 = 5;
+var b1 = 0;
+var c1 = -3;
+var d1 = 2;
+
+
+// Задание 11
+if (a1 > 0 && a1 < 5) alert('При ' + a1 + ' условие верно!'); 
+else alert ('При ' + i + ' условие неверно!');
+
+if (b1 > 0 && b1 < 5) alert('При ' + b1 + ' условие верно!');
+else alert ('При ' + a + ' условие неверно!');
+
+if (c1 > 0 && c1 < 5) alert('При ' + c1 + ' условие верно!'); 
+else alert ('При ' + a + ' условие неверно!');
+
+if (d1 > 0 && d1 < 5) alert('При ' + d1 + ' условие верно!'); 
+else alert ('При ' + a + ' условие неверно!');
+
+
+// Задание 12
+if (a1 == 0 || a1 == 2) alert (a1 * 7);
+ else alert (a1 / 10);
+
+if (b1 == 0 || b1 == 2) alert (b1 * 7);
+ else alert (b1 / 10);
+
+if (c1 == 0 || c1 == 2) alert (c1 * 7);
+ else alert (c1 / 10);
+
+if (d1 == 0 || d1 == 2) alert (d1 * 7); 
+else alert (d1 / 10);
+
+
+// Задание 13
+var a2 = 1;
+var b2 = 3;
+var c2 = 0;
+var d2 = 6;
+var e2 = 3;
+var f2 = 5;
+
+if (a2 <= 1 || b2 >= 3) alert (a2 + b2); 
+else (a2 - b2);
+
+if (c2 <= 1 || d2 >= 3) alert (c2 + d2); 
+else (c2 - d2);
+
+if (e2 <= 1 || f2 >= 3) alert (e2 + f2); 
+else (e2 - f2);
+
+
+// Задание 14
+var a3 = 7;
+var b3 = 12;
+if (a3 > 2 && a3 < 11 || b3 >= 6 && b3 < 14) alert('верно'); else ('Неверно');
+
+
+// Задание 15
+var num = 4;
+var result;
+
+switch (num) {
+    case 1:
+        result = 'Зима';
+        alert(result);
+    break;
+    case 2: 
+        result = 'Весна';   
+        alert(result);
+    break;
+    case 3: 
+        result = 'Лето';   
+        alert(result);
+    break;
+    case 4: 
+        result = 'Осень';   
+        alert(result);
+    break;
+}
+
+
+
+//Задание 16
+var day = 21;
+var decade;
+
+if (day <= 10 ) {
+    decade = 1;
+} else if (day >= 11 && day <=20) {
+    decade = 2;
+} else {
+    decade = 3;
+}
+
+alert('Число ' + day + ' попадает в ' + decade + ' декаду');
+
+
+// Задание 17
+
+var month = prompt('Введите номер месяца');
+var season;
+
+if (month == 12 || month < 3) {
+    season = 'Зима'; 
+} else if (month > 2 && month < 6) {
+    season = 'Весна';
+} else if (month > 5 && month < 9) {
+    season = 'Лето';
+} else if (month > 8 && month < 12) {
+    season = 'Осень';
+} else {alert('Некорретно введен номер месяца')}
+alert(season);
+
+
+// Задание 18
+var str = 'abcde';
+
+if (str[0] == 'a') {
+    alert('Да');
+} 
+else {
+    alert('Нет');
+}
+
+
+// Задание 19
+var str1 = '12345';
+
+if (str1[0] == '1' || str1[0] == '2' || str1[0] == '3') {
+    alert('Да');
+} else {
+    alert('Нет');
+}
+
+
+// Задание 20
+var str2 = '123';
+var sum = Number(str2[0]) + Number(str2[1]) + Number(str2[2]);
+alert(sum);
+
+
+// Задание 21
+var str3 = '123456';
+var summ1 = Number(str3[0]) + Number(str3[1]) + Number(str3[2]);
+var summ2 = Number(str3[3]) + Number(str3[4]) + Number(str3[5]);
+if (summ1 == summ2) {
+    alert('Да');
+} else {
+    alert('Нет');
+}
